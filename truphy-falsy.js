9d4a926af43afd4c744cc0790fa30fb8887dc337
Samuel Alessandro <samuel.alessandro.santos@gmail.com>
2022-11-01 11:24:03 -0400

    váriaveis / true e false

---
+++ truphy-falsy.js
@@ -0,0 +1,19 @@
+//boolean 
+const usuarioLogado = true
+const contaPaga = false
+
+//truphy or falsy
+
+//0 => false
+//1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+//null ==> vazio/nada
+
+let minhaVar
+let varNull = null
+console.log(minhaVar)
+console.log(varNull)
---
+++ váriaveis.js
@@ -0,0 +1,38 @@
+//exemplo VAR
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+// var area;
+
+//exemplo LET
+
+// let forma = 'retangulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area
+
+// if(forma  === 'retangulo') {
+//     area = altura * comprimento
+// } else{
+//     area = (altura * comprimento) / 2 
+// }
+
+// console.log(area)
+
+//exemplo CONST 
+
+const forma = 'triangulo'
+const altura = 5
+const comprimento = 7
+let area
+
+if (forma === 'quadrado') {
+    area = altura * comprimento
+} else {
+    area = (altura * comprimento) / 2
+}
+
+console.log(area)
