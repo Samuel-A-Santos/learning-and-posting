5e1dce3320dfc0470cd3bd2b15e22d6a1a6bc4f8
Samuel Alessandro <samuel.alessandro.santos@gmail.com>
2022-11-01 10:18:07 -0400

    Primeiras aulas

---
+++ strings.js
@@ -0,0 +1,12 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhasupersegura456";
+
+const citacao = "Meu nome é ";
+const meuNome = "Samuel";
+
+// concatencao com + para strings
+
+console.log(citacao + meuNome)
+
+// template string ou template literal
