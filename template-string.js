d9e709ed802b933873af8b02de638fc30ac0abe6
Samuel Alessandro <samuel.alessandro.santos@gmail.com>
2022-11-07 21:21:08 -0400

    template-string

---
+++ template-string.js
@@ -0,0 +1,11 @@
+const nome = "Samuel";
+const idade = 2022-2004;
+const cidadeDeNascimento = "Minas Gerais";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
+
+//template string é uma forma mais facil de escrever e mais organizada para quem le o codigo, é usado o acento `crase
